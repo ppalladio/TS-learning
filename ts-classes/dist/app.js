873d712e7b4d507bb94528fe85ceb7933f41f175
ppalladio <nine.slide@gmail.com>
2022-11-10 23:58:53 +0100

    interfaces with classes

--- ts-classes/dist/app.js
+++ ts-classes/dist/app.js
@@ -1,11 +1,16 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'John',
-    age: 25,
+class Person {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
     greet(phrase) {
         console.log(phrase + ' ' + this.name);
-    },
-};
+    }
+}
+let user1;
+user1 = new Person('John', 30);
 user1.greet('whatever');
 //# sourceMappingURL=app.js.map
--- ts-classes/src/app.ts
+++ ts-classes/src/app.ts
@@ -1,18 +1,27 @@
-interface Person {
+interface Greeting {
     name: string;
     age: number;
 
     greet(phrase: string): void; //' only the description of method
 }
 
-let user1: Person;
-
-user1 = {
-    name: 'John',
-    age: 25,
+class Person implements Greeting {
+    constructor(public name: string, public age: number) {}
+    //'while doing shorthand initialization, it is a must to state public and private
     greet(phrase: string) {
         console.log(phrase + ' ' + this.name);
-    },
-};
+    }
+}
+
+let user1: Greeting;
+
+user1 = new Person('John', 30);
+// user1 = {
+//     name: 'John',
+//     age: 25,
+//     greet(phrase: string) {
+//         console.log(phrase + ' ' + this.name);
+//     },
+// };
 
 user1.greet('whatever');
