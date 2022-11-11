c0c20d64f1dfbf0e661acc2a491579eb4cec0c05
Yuxuan <nine.slide@gmail.com>
2022-11-11 15:59:05 +0100

    Define fn with interface/Optional(?)

--- ts-classes/src/app.ts
+++ ts-classes/src/app.ts
@@ -1,5 +1,19 @@
+// type addFn=(a:number, b:number)=>number //' define a function with type
+
+interface addFn {
+    (a: number, b: number): number;
+} //' define a function with interface
+
+let add: addFn;
+
+add = (n1: number, n2: number) => {
+    return n1 + n2;
+};
+
 interface Named {
-    name: string;
+    name?: string; //' with name being optional we can make the name in class optional too
+    outputName?: string; //' it would be optional and wont be enforced on classes that use this interface
+    sayMyName?(): void; //' same thing with methods
 }
 
 interface Greeting extends Named {
@@ -9,7 +23,7 @@ interface Greeting extends Named {
 }
 
 class Person implements Greeting {
-    constructor(public name: string, public age: number) {}
+    constructor(public name: /*?*/ string, public age: number) {} //' a required parameter cant be followed by optional parameter
     //'while doing shorthand initialization, it is a must to state public and private
     greet(phrase: string) {
         console.log(phrase + ' ' + this.name);
