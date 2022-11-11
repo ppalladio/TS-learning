00713d4aade5e54653b0a9b56d2a3960711a9d7f
Yuxuan <nine.slide@gmail.com>
2022-11-11 17:50:44 +0100

    intersect and combine

--- advanced-types/src/app.ts
+++ advanced-types/src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+type Admin = {
+    name: string;
+    privileges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+type PromoteEmployee = Admin &
+    /* if | is used then we dont need all three properties */ Employee; //'combine two types
+
+const e1: PromoteEmployee = {
+    name: 'ana',
+    privileges: ['be a boss'],
+    startDate: new Date(),
+};
+
+type type1 = number | string;
+type type2 = number | boolean;
+
+type intersect = type1 & type2; //' type intersect: number
