ce1b7433d774813c01bd1c1c84593ec8a68fb595
ppalladio <nine.slide@gmail.com>
2022-11-12 23:59:33 +0100

    builtin Generics

--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -1,1 +1,10 @@
-// Code goes here!
+const name1: string[] = ['ana'];
+const name2 = []; //' any[]
+// const names:Array=[]//'Generic type 'Array<T>' requires 1 type argument(s)
+const name3: Array<string> = []; //' === name3:string[]
+
+const promise: Promise<string> = new Promise((res, _rej) => {
+    setTimeout(() => {
+        res('timeout');
+    }, 2000);
+});
