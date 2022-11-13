55633ab6e2d5077407e956ebc8a1f43eaf93f1bd
ppalladio <nine.slide@gmail.com>
2022-11-13 17:02:03 +0100

    keyof

--- Generics/dist/app.js
+++ Generics/dist/app.js
@@ -9,4 +9,29 @@ mergeObj2.hobbies.forEach((el) => {
     console.log(el);
 });
 console.log(mergeObj2.hobbies[0]);
+function countNPrint(el) {
+    let description = 'has no element';
+    if (el.length > 0) {
+        description = 'got' + el.length + ' element(s)';
+    }
+    return [el, description];
+}
+const count1 = countNPrint('text');
+const count2 = countNPrint(['test1', 'test2']);
+const count3 = countNPrint([]);
+console.log(count1);
+console.log(count2);
+console.log(count3);
+function fn1(obj, key) {
+    return obj[key];
+}
+const Person = {
+    name: 'John',
+    age: 30,
+    address: '123 Main St',
+    occupation: 'manager',
+    dicovoced: true,
+};
+const result = fn1(Person, 'dicovoced');
+console.log(result);
 //# sourceMappingURL=app.js.map
--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -25,3 +25,43 @@ mergeObj2.hobbies.forEach((el) => {
  */
 
 console.log(mergeObj2.hobbies[0]); //. eat
+
+//: another generic function
+
+interface Lengthy {
+    length: number;
+}
+//. we only care that it has a length property because we rely on the length property in the function
+function countNPrint<T extends Lengthy>(el: T): [T, string] {
+    //' to specify that it will return a tuple
+    let description = 'has no element';
+    if (el.length > 0) {
+        description = 'got' + el.length + ' element(s)';
+    }
+    return [el, description];
+}
+
+const count1 = countNPrint('text');
+const count2 = countNPrint(['test1', 'test2']);
+const count3 = countNPrint([]);
+console.log(count1);
+console.log(count2);
+console.log(count3);
+
+//: keyof contraint
+
+function fn1<T extends object, U extends keyof T>(obj: T, key: U) {
+    return obj[key];
+}
+
+const Person = {
+    name: 'John',
+    age: 30,
+    address: '123 Main St',
+    occupation: 'manager',
+    dicovoced: true,
+};
+
+const result = fn1(Person, 'dicovoced');
+
+console.log(result);
