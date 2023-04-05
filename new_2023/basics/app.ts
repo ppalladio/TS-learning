5e91f862791e79aedeedf1ecddfac454ec2d8af7
ppalladio <nine.slide@gmail.com>
2023-04-05 22:39:36 +0200

    tuple

--- new_2023/basics/app.ts
+++ new_2023/basics/app.ts
@@ -4,10 +4,18 @@
 //	hobbies:string[]; //>string array
 // }
 
-const person = {
+const person :{
+	name:string;
+	age:number;
+	hobbies:string[];
+	role:[number, string]; //tuple length cant be changed, but push can add. type cant be changed.
+} = {
     name: 'John',
     age: 20,
 	hobbies: ['coding', 'programming'],
+	role:[1,'prof']
 };
+
+// person.role=[] //@not allowed
 
 console.log(person.name);
