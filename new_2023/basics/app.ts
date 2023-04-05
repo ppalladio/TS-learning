81a36585546175ca4fd133102942e89e98f9b500
ppalladio <nine.slide@gmail.com>
2023-04-05 22:36:10 +0200

    array

--- new_2023/basics/app.ts
+++ new_2023/basics/app.ts
@@ -1,11 +1,13 @@
 // const person :{}={
 // 	name:string;
 // 	age:number;
+//	hobbies:string[]; //>string array
 // }
 
 const person = {
     name: 'John',
     age: 20,
+	hobbies: ['coding', 'programming'],
 };
 
 console.log(person.name);
