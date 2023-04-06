c38f634012bcd7f55a8cbd75c173f14ef97e617f
Yuxuan Peng <nine.slide@gmail.com>
2023-04-06 14:35:18 +0200

    literal type

--- new_2023/basics/app.ts
+++ new_2023/basics/app.ts
@@ -1,15 +1,20 @@
-function add(input1: number | string, input2: number | string) {
+const add = (
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-num' | 'as-str', //@literal type and union type, only these strings are allowed for this variable;
+) => {
     let result;
-    if (input1 === 'number' && input2 === 'number') {
-        result = input1 + input2;
+    if (
+        (input1 === 'number' && input2 === 'number') ||
+        resultConversion === 'as-num'
+    ) {
+        result = +input1 + +input2;
     } else {
-       result = input1.toString() + input2.toString();
+        result = input1.toString() + input2.toString();
     }
     return result;
-}
+};
 
-console.log(add(1, 2));
-
-console.log(add('number', 'string'));
-
-
+console.log(add(1, 2, 'as-num'));
+console.log(add('1', '2', 'as-num'));
+console.log(add('number', 'string', 'as-str'));
