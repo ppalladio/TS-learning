93b63ad5a78fa98513476085da4dd9a1158873de
ppalladio <nine.slide@gmail.com>
2023-04-07 21:07:31 +0200

    rest operator

--- new_2023/ES6_in_TS/dist/app.js
+++ new_2023/ES6_in_TS/dist/app.js
@@ -1,3 +1,14 @@
 "use strict";
-const printOutput = (a, b) => console.log(a + b);
+const printOutput = (a, b = 1) => console.log(a + b); //@ default value can only be assigned to after type declaration 
 printOutput(1, 2);
+const hobbies = ["sports", "tennis", "dream"];
+const activeHobbies = ["hiking"];
+const person = {
+    name: "Joe",
+    age: 3
+};
+const infoPerson = Object.assign(Object.assign({}, person), { hobbies: "acting" });
+activeHobbies.push(...hobbies);
+console.log(...hobbies);
+console.log(activeHobbies);
+console.log(infoPerson);
--- new_2023/ES6_in_TS/src/app.ts
+++ new_2023/ES6_in_TS/src/app.ts
@@ -1,4 +1,18 @@
+const printOutput: (a: number, b: number) => void = 
+(a, b = 1) => console.log(a + b); //@ default value can only be assigned to after type declaration 
 
-const printOutput: (a: number, b: number) => void = (a, b) => console.log(a+b);
+printOutput(1, 2);
 
-printOutput(1, 2); 
+
+const hobbies = ["sports", "tennis", "dream"];
+const activeHobbies = ["hiking"]
+const person={
+	name: "Joe",
+	age:3
+}
+
+const infoPerson = {...person,hobbies:"acting"}
+activeHobbies.push(...hobbies);
+console.log(...hobbies);
+console.log(activeHobbies);
+console.log(infoPerson);
