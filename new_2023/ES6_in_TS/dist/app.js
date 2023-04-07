9ff9411bdd4d16dd7384421f7042b6f58b57653b
ppalladio <nine.slide@gmail.com>
2023-04-07 20:48:52 +0200

    function type void and never

---
+++ new_2023/ES6_in_TS/dist/app.js
@@ -0,0 +1,3 @@
+"use strict";
+const printOutput = (a, b) => console.log(a + b);
+printOutput(1, 2);
---
+++ new_2023/ES6_in_TS/src/app.ts
@@ -0,0 +1,4 @@
+
+const printOutput: (a: number, b: number) => void = (a, b) => console.log(a+b);
+
+printOutput(1, 2); 
