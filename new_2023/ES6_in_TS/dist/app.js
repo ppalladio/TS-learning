d0cf79bd6ed1a0e3e78975ddb0336cd16105e110
ppalladio <nine.slide@gmail.com>
2023-04-07 21:13:46 +0200

    rest operator

--- new_2023/ES6_in_TS/dist/app.js
+++ new_2023/ES6_in_TS/dist/app.js
@@ -1,14 +1,20 @@
 "use strict";
-const printOutput = (a, b = 1) => console.log(a + b); //@ default value can only be assigned to after type declaration 
+const printOutput = (a, b = 1) => console.log(a + b); //@ default value can only be assigned to after type declaration
 printOutput(1, 2);
-const hobbies = ["sports", "tennis", "dream"];
-const activeHobbies = ["hiking"];
+const hobbies = ['sports', 'tennis', 'dream'];
+const activeHobbies = ['hiking'];
 const person = {
-    name: "Joe",
-    age: 3
+    name: 'Joe',
+    age: 3,
 };
-const infoPerson = Object.assign(Object.assign({}, person), { hobbies: "acting" });
+const infoPerson = Object.assign(Object.assign({}, person), { hobbies: 'acting' });
 activeHobbies.push(...hobbies);
 console.log(...hobbies);
 console.log(activeHobbies);
 console.log(infoPerson);
+const add = (...num) => {
+    return num.reduce((cur, val) => {
+        return cur + val;
+    }, 0);
+};
+console.log(add(1, 2, 3));
--- new_2023/ES6_in_TS/src/app.ts
+++ new_2023/ES6_in_TS/src/app.ts
@@ -1,18 +1,24 @@
-const printOutput: (a: number, b: number) => void = 
-(a, b = 1) => console.log(a + b); //@ default value can only be assigned to after type declaration 
+const printOutput: (a: number, b: number) => void = (a, b = 1) =>
+    console.log(a + b); //@ default value can only be assigned to after type declaration
 
 printOutput(1, 2);
 
+const hobbies = ['sports', 'tennis', 'dream'];
+const activeHobbies = ['hiking'];
+const person = {
+    name: 'Joe',
+    age: 3,
+};
 
-const hobbies = ["sports", "tennis", "dream"];
-const activeHobbies = ["hiking"]
-const person={
-	name: "Joe",
-	age:3
-}
-
-const infoPerson = {...person,hobbies:"acting"}
+const infoPerson = { ...person, hobbies: 'acting' };
 activeHobbies.push(...hobbies);
 console.log(...hobbies);
 console.log(activeHobbies);
 console.log(infoPerson);
+
+const add = (...num: number[]): number => {
+    return num.reduce((cur, val) => {
+        return cur + val;
+    }, 0);
+};
+console.log(add(1, 2, 3));
